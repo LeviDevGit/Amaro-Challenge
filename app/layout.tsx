aae1b77a2d87306909f95235e6338bd57a72eb23
Levi <114665569+LeviGit-web@users.noreply.github.com>
2023-11-09 00:37:03 -0300

    feat: start project with nextjs, organization and fonts application

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Open_Sans } from 'next/font/google'
+import localFont from 'next/font/local'
+
+const gothamBook = localFont({ src: '../public/fonts/Gotham Book.otf' })
+export const openSans = Open_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Amaro Challenge'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={gothamBook.className}>{children}</body>
+    </html>
+  )
+}
