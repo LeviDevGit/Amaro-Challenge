2149435f5c0f72ca64936557bec38daa658dc9c9
Levi <114665569+LeviGit-web@users.noreply.github.com>
2023-11-28 17:03:25 -0300

    feat: dinamic page & home page updated

--- app/c/[styleId]/page.tsx
+++ app/c/[styleId]/page.tsx
@@ -1,7 +1,46 @@
-export default function Product({ params }) {
+"use client";
+
+import { useParams } from "next/navigation";
+import database from "@/app/services/products.json";
+import styles from "./styles.module.css";
+
+export default function Product() {
+  const params = useParams();
+  const uid = params.styleId;
+
+  const product = database.products.find((element) => element.style == uid);
+
   return (
-    <div>
-      <h1>{params.styleId}</h1>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <img src={product?.image} alt="" />
+        <div className={styles.text}>
+          <h1>{product?.name}</h1>
+          <div className={styles.price}>
+            <h2>{product?.actual_price}</h2>
+            <span>{product?.installments}</span>
+          </div>
+          <div className={styles.color}>
+            <h2>
+              Cor: <span>{product?.color_slug}</span>
+            </h2>
+          </div>
+          <div className={styles.size}>
+            <h2>Tamanho:</h2>
+            <div className={styles.sizeChoice}>
+              {true
+                ? product?.sizes.map((element, index) => {
+                    return (
+                      <div key={index}>
+                        <button>{element.size}</button>
+                      </div>
+                    );
+                  })
+                : null}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- app/components/Main/MainComponent.tsx
+++ app/components/Main/MainComponent.tsx
@@ -9,27 +9,30 @@ export default function MainComponent() {
   const router = useRouter();
 
   const handleButtonClick = (style: string) => {
-    console.log(style);
     router.push(`/c/${style}`);
   };
 
   return (
     <main className={styles.container}>
-      <h1>Mais vendidos em Moda</h1>
+      <h1>Mais vendidos em Moda Feminina</h1>
       <div className={styles.list}>
         <ul>
           {true
-            ? database.products.map((products) => {
+            ? database.products.map((product, index) => {
+                if (product.image == "")
+                  product.image =
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/800px-Image_not_available.png?20210219185637";
+
                 return (
-                  <li className={styles.card} key={products.name}>
+                  <li className={styles.card} key={index}>
                     <button
                       onClick={() => {
-                        handleButtonClick(products.style);
+                        handleButtonClick(product.style);
                       }}
                     >
-                      <Image src={products.image} alt={products.name} />
+                      <img src={product.image} alt={product.name} />
 
-                      <h2>{products.name}</h2>
+                      <h2>{product.name}</h2>
                       <div className={styles.discount}>
                         <h3>R$ 99,90</h3>
                         <span>R$ 199,90</span>
